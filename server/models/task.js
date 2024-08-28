e74e43ecce660019b86c693d66408ca62acdc7cf
ycmkat <kattyyu29@gmail.com>
2024-08-27 23:50:25 -0400

    Create task schema with id and title fields

---
+++ server/models/task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// create schema for Task
+const Task = new mongoose.Schema({
+    // id and title
+    id: {  // alternatively, use default _id field from mongoose
+        type: String,
+        required: true,
+        unique: true  // primary key
+    },
+    title: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Task', Task)
