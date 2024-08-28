b6fc3a9e731488dad181c961dd42f86928aad48f
ycmkat <kattyyu29@gmail.com>
2024-08-28 01:08:52 -0400

    Create task list component and add routing to App.js

---
+++ client/src/components/TaskList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TaskList = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        // get all tasks
+        const fetchTasks = async () => {
+            try {
+                const response = await axios.get('/api/tasks');
+                setTasks(response.data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+        fetchTasks();
+    }, []);
+
+    return (
+        <div>
+            <h1>Task List</h1>
+            <ul>
+                {tasks.map((tasks) => (
+                    <li key={tasks.id}>{tasks.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+export default TaskList;
