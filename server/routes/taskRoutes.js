801025ed13ed6a9f6909a6480fe2cda0c26815b9
ycmkat <kattyyu29@gmail.com>
2024-08-29 00:25:50 -0400

    Implement DELETE route to delete an existing task

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -40,4 +40,16 @@ router.patch('/api/task/:id', async (req, res) => {
     }
 });
 
+// DELETE /api/tasks/:id: Delete an existing task
+router.delete('/api/task/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({message: 'Task not found'});
+        await task.remove();
+        res.status(200).json({message: 'Task deleted'});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+});
+
 module.exports = router;
