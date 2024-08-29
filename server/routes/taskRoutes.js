3ff0a352e4a5f89f9949b8cc18f5b847c417179f
ycmkat <kattyyu29@gmail.com>
2024-08-29 00:17:01 -0400

    Implement POST route to create new tasks

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Task = require('../models/task');
 
 // GET /api/tasks: Retrieve all tasks
-router.get('/tasks', async (req, res) => {
+router.get('/api/tasks', async (req, res) => {
     try {
         const tasks = await Task.find();
         res.json(tasks);
@@ -11,5 +11,18 @@ router.get('/tasks', async (req, res) => {
         res.status(500).json({message: err.message});
     }
 });
+
+// POST /api/tasks: Create a new task
+router.post('/api/tasks', async (req, res) => {
+    const task = new Task({
+        title: req.body.title,
+    });
+    try {
+        const newTask = await task.save();
+        res.status(201).json(newTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message});  // Bad request
+    }
+})
 
 module.exports = router;
